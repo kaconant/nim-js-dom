723432e84c0e7707ecc4be964fab1f0ea0ac77d8
Krissy Conant <krissy@memberclicks.com>
2018-04-10 21:22:58 -0400

    started javascript to jquery in js file

--- nim.js
+++ nim.js
@@ -1,128 +1,124 @@
+/*
+1) Add the<script> tag that loads in jQuery from code.jquery.com
+
+2) Modify the DOM element variables in the STATE section to be jQuery elements
+
+3) Update the renderGame() function to use jQuery
+
+4) Rewrite the onclick methods in the style of jQuery
+*/
+
+
 //***************
 //  STATE
 //***************
-var chips = 16;
-var playerOneChips = 0;
-var playerTwoChips = 0;
-var currentPlayer = 1;
-var chipsTakenThisTurn = 0;
-var playerOneName = '';
-var playerTwoName = '';
-
-// A string representing the html for a chip
-var chipHTML = '<div class="chip"></div>'; 
-
-// Variables that point to different DOM elements
-var chipContainer =    document.getElementById('chips');
-var playerOneChipsContainer =   document.getElementById('player-one-chips');
-var playerTwoChipsContainer =   document.getElementById('player-two-chips');
-var playerOneTake =    document.getElementById('player-one-take');
-var playerTwoTake =    document.getElementById('player-two-take');
-var playerOnePass =    document.getElementById('player-one-pass');
-var playerTwoPass =    document.getElementById('player-two-pass');
-var playerOneOptions = document.getElementById('player-one-options');
-var playerTwoOptions = document.getElementById('player-two-options');
-var playerOneName =    document.getElementById('player-one-name');
-var playerTwoName =    document.getElementById('player-two-name');
+$(function() {
+  var chips = 16;
+  var playerOneChips = 0;
+  var playerTwoChips = 0;
+  var currentPlayer = 1;
+  var chipsTakenThisTurn = 0;
+  var playerOneName = '';
+  var playerTwoName = '';
+  var chipHTML = '<div class="chip"></div>';
+  var chipContainer = $('#chips');
+  var playerOneChipsContainer = $('#player-one-chips');
+  var playerTwoChipsContainer = $('#player-two-chips');
+  var playerOneTake = $('#player-one-take');
+  var playerTwoTake = $('#player-two-take');
+  var playerOnePass = $('#player-one-pass');
+  var playerTwoPass = $('#player-two-pass');
+  var playerOneOptions = $('#player-one-options');
+  var playerTwoOptions = $('#player-two-options');
+  var playerOneName = $('#player-one-name');
+  var playerTwoName = $('#player-two-name');
 
 //*****************
 // ACTIONS
 //*****************
 
-function renderGame() {
-  // First, draw all the chips into the right containers
-  chipContainer.innerHTML = '';
-  playerOneChipsContainer.innerHTML = '';
-  playerTwoChipsContainer.innerHTML = '';
-  for (var i=0; i<chips; i++) {
-    chipContainer.innerHTML += chipHTML
-  };
-  for (var i=0; i<playerOneChips; i++) {
-    playerOneChipsContainer.innerHTML += chipHTML
-  };
-  for (var i=0; i<playerTwoChips; i++) {
-    playerTwoChipsContainer.innerHTML += chipHTML
-  };
+  var renderGame = function() {
+    // First, draw all the chips into the right containers
+    $(chipContainer).html();
+    $(playerOneChipsContainer).html();
+    $(playerTwoChipsContainer).html();
+    $(chipContainer).append(chipHTML);
+    $(playerOneChipsContainer).append(chipHTML);
+    $(playerTwoChipsContainer).append(chipHTML);
+    
+    // Show/Hide the appropriate action buttons
+    $(currentPlayer).toggle();
 
-  // Show/Hide the appropriate action buttons
-  if (currentPlayer == 1) {
-    playerOneOptions.style.visibility = "visible"; 
-    playerTwoOptions.style.visibility = "hidden"; 
-  } else {
-    playerOneOptions.style.visibility = "hidden"; 
-    playerTwoOptions.style.visibility = "visible"; 
+    // Show the name of each player
+    $(playerOneName).html(playerOne);
+    $(playerTwoName).html(playerTwo);
+
+***************************
+
+  var takeChip = function(){
+    var message = ("You can't take any more chips!")
+    $(chipsTakenThisTurn == 3).alert(message);
+
+    }
+    chips--;
+    chipsTakenThisTurn++;
+    
+    if (currentPlayer == 1) {
+      playerOneChips++;
+    } else {
+      playerTwoChips++;
+    }
+
+    if (chips == 0) {
+      alert("Game Over!");
+      resetGame();
+    }
+
+    renderGame();
   }
 
-  // Show the name of each player
-  playerOneName.innerHTML = playerOne;
-  playerTwoName.innerHTML = playerTwo;
-}
+  function pass() {
+    if (chipsTakenThisTurn == 0) {
+      alert('hey, you need to take at least one');
+      return;
+    }
+    currentPlayer = (currentPlayer + 1) % 2;
+    chipsTakenThisTurn = 0; 
 
-function takeChip() {
-  
-  if (chipsTakenThisTurn == 3) {
-    alert("you can't take any more chips!");
-    return;
-  }
-  chips--;
-  chipsTakenThisTurn++;
-  
-  if (currentPlayer == 1) {
-    playerOneChips++;
-  } else {
-    playerTwoChips++;
+    renderGame();
   }
 
-  if (chips == 0) {
-    alert("Game Over!");
-    resetGame();
+  function resetGame() {
+    chips = 16;
+    chipsTakenThisTurn = 0;
+    playerOneChips = 0;
+    playerTwoChips = 0;
+    currentPlayer = 1;
+    playerOne = prompt("Enter player one's name");
+    playerTwo = prompt("Enter player two's name");
+    renderGame();
+    
   }
 
-  renderGame();
-}
-
-function pass() {
-  if (chipsTakenThisTurn == 0) {
-    alert('hey, you need to take at least one');
-    return;
+  //***********************
+  //  KICKING IT ALL OFF
+  //***********************
+  playerOneTake.onclick = function () {
+    takeChip();
   }
-  currentPlayer = (currentPlayer + 1) % 2;
-  chipsTakenThisTurn = 0; 
 
-  renderGame();
-}
+  playerTwoTake.onclick = function() {
+    takeChip();
+  }
 
-function resetGame() {
-  chips = 16;
-  chipsTakenThisTurn = 0;
-  playerOneChips = 0;
-  playerTwoChips = 0;
-  currentPlayer = 1;
-  playerOne = prompt("Enter player one's name");
-  playerTwo = prompt("Enter player two's name");
-  renderGame();
-  
-}
+  playerOnePass.onclick = function() {
+    pass();
+  }
 
-//***********************
-//  KICKING IT ALL OFF
-//***********************
-playerOneTake.onclick = function () {
-  takeChip();
-}
-
-playerTwoTake.onclick = function() {
-  takeChip();
-}
-
-playerOnePass.onclick = function() {
-  pass();
-}
-
-playerTwoPass.onclick = function() {
-  pass();
-}
+  playerTwoPass.onclick = function() {
+    pass();
+  }
 
 
 
-resetGame();
+  resetGame();
